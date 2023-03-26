2b889415c9ed39bc6d9e51154042e748643b7623
JorbeJr <50496289+Jorbee1234@users.noreply.github.com>
2023-03-26 09:56:16 -0300

    Estudo de interface

---
+++ Bloco 2 - Conhecimento para adquirir/PraticandoExpection/Interface/Interface/Entities/Invoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface.Entities
+{
+    public class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice()
+        {
+
+        }
+
+        public Invoice(double rental, double tax)
+        {
+            BasicPayment = rental;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return @$"Basic payment: {BasicPayment.ToString("F2", CultureInfo.InvariantCulture)}
+                + \nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
